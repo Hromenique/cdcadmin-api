8bb78ff8296f16a764dd3089f0dde445c1169a4f
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2018-06-04 22:05:17 -0300

    troca do banco para o banco em memoria hsql e carga inicial

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into autor (email, nome, senha) values ('fulano@gmail.com', 'Fulano de tal', '12345');
+insert into autor (email, nome, senha) values ('goku@gmail.com', 'Son Goku', '12345');
+insert into autor (email, nome, senha) values ('naruto@gmail.com', 'Naruto Uzumaki', '12345');
+
+insert into livro (autor_id, preco, titulo) values (1, 100.0, 'Como ser um Fulano');
+insert into livro (autor_id, preco, titulo) values (2, 200.0, 'Como se fazer um Kamehameha');
+insert into livro (autor_id, preco, titulo) values (3, 100.0, 'Manual do ninja moderno');
